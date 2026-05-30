a0566f93ba2b676b634793a951a53c8f4139a56c
operacionesquasar23-netizen <operacionesquasar23@gmail.com>
2026-05-30 17:38:17 -0500

    Fix duplicacion evidencias

--- src/pages/api/solicitudes/nueva.js
+++ src/pages/api/solicitudes/nueva.js
@@ -59,7 +59,7 @@ export default async function handler(req, res) {
       '',                   // FechaCierre
       '',                   // Responsable
       fileLinks,            // DatosAdjuntos
-      imageLinks            // ImageLinks
+      ''                    // Evidencias
     ])
 
     await appendRow('Historial', [
