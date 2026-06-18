ae1a02ec7c976e5422d739dc452d4bf23e35efa1
operacionesquasar23-netizen <operacionesquasar23@gmail.com>
2026-06-17 22:38:51 -0500

    Prueba sin supabase

--- src/pages/api/solicitudes/avanzar.js
+++ src/pages/api/solicitudes/avanzar.js
@@ -1,4 +1,4 @@
-import { supabaseAdmin } from '../../../lib/supabase'
+//import { supabaseAdmin } from '../../../lib/supabase'
 import { sendStatusUpdate } from '../../../lib/email'
 
 export default async function handler(req, res) {
